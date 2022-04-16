8d305c4764e77972bb12e35746c52d0aa49d7ee3
asia flowers <90466041+asiaflowers@users.noreply.github.com>
2022-04-16 10:46:07 -0500

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,10 @@
+import main
+
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    assert 'Hello phase four' in r.data.decode('utf-8')
