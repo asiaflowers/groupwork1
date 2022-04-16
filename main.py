7416ce41f75bd5d12a02a513947a480cd491b36d
asia flowers <90466041+asiaflowers@users.noreply.github.com>
2022-04-16 10:50:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello phase 4!'
+    return 'Hello phase four!'
 
 
 if __name__ == '__main__':
